4640a2e2aaa46eb2f1f0245fb9502e301c70209b
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2024-06-08 10:24:29 +0900

    add guideline15

---
+++ src/Guideline15/execute.hh
@@ -0,0 +1,8 @@
+#ifndef SRC_GUIDELINE15_EXECUTE_HH_
+#define SRC_GUIDELINE15_EXECUTE_HH_
+
+
+namespace Guideline15 {
+void execute();
+};  // namespace Guideline15
+#endif  // SRC_GUIDELINE15_EXECUTE_HH_
