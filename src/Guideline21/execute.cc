91d26ead95a9658342b68a24b41c7e76e2266815
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2024-06-23 13:47:00 +0900

    add guideline21 code base

---
+++ src/Guideline21/execute.cc
@@ -0,0 +1,9 @@
+// Copyright saito 2024
+#include <iostream>
+
+
+namespace Guideline21 {
+void execute() {
+  std::cout << "Guideline21" << std::endl;
+}
+};  // namespace Guideline21
---
+++ src/Guideline21/execute.hh
@@ -0,0 +1,9 @@
+// Copyright saito 2024
+#ifndef SRC_GUIDELINE21_EXECUTE_HH_
+#define SRC_GUIDELINE21_EXECUTE_HH_
+
+
+namespace Guideline21 {
+void execute();
+};  // namespace Guideline21
+#endif  // SRC_GUIDELINE21_EXECUTE_HH_
